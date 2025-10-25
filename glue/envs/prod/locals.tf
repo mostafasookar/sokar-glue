94738bac50aeafb6645a418cc0aeabf474c5bbd5
Moustafa Sokkar <mostafasookar@hotmail.com>
2025-10-25 22:58:37 +0400

    update locals file to fix issue in manager

--- glue/envs/prod/locals.tf
+++ glue/envs/prod/locals.tf
@@ -1,37 +1,32 @@
 locals {
-  _job_defaults = {
-    description        = ""
-    glue_version       = "5.0"
-    python_version     = "3"
-    command_name       = "glueetl"
-    worker_type        = "G.1X"
-    number_of_workers  = 2
-    timeout            = 300
-    max_retries        = 0
-    connections        = []
-    default_arguments  = {}
-    tags               = {}
-    trigger            = { enabled = false }
-  }
+  # Parse YAML jobs
+  raw_jobs = yamldecode(file("${path.module}/../../jobs.yaml"))
 
+  # Normalize each job
   jobs = [
-    for job in yamldecode(file("${path.module}/../../jobs.yaml")) :
-    merge(
-      local._job_defaults,
-      job,
-      {
-        default_arguments = merge(
-          {
-            "--TempDir"               = "s3://${var.scripts_bucket}/temporary/"
-            "--spark-event-logs-path" = "s3://${var.scripts_bucket}/sparkHistoryLogs/"
-          },
-          lookup(job, "default_arguments", {})
-        ),
-        trigger = merge(
-          { enabled = false },
-          try(job.trigger, {})
-        )
+    for job in local.raw_jobs : {
+      name              = job.name
+      description       = try(job.description, "")
+      script_file       = try(job.script_file, "")
+      glue_version      = try(job.glue_version, "5.0")
+      python_version    = try(job.python_version, "3")
+      command_name      = try(job.command_name, "glueetl")
+      worker_type       = try(job.worker_type, "G.1X")
+      number_of_workers = try(job.number_of_workers, 2)
+      connections       = try(job.connections, [])
+      default_arguments = merge(
+        {
+          "--TempDir"               = "s3://${var.scripts_bucket}/temporary/"
+          "--spark-event-logs-path" = "s3://${var.scripts_bucket}/sparkHistoryLogs/"
+        },
+        try(job.default_arguments, {})
+      )
+      tags = try(job.tags, {})
+      trigger = {
+        enabled  = try(job.trigger.enabled, false)
+        type     = try(job.trigger.type, "")
+        schedule = try(job.trigger.schedule, "")
       }
-    )
+    }
   ]
 }
